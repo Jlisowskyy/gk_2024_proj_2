2c8f21148e7f805f6cbf11cbc5b009503585ac70
Jlisowskyy <Jlisowskyy@gmail.com>
2024-11-09 13:26:39 +0100

    Added own bitmap

--- app/include/Rendering/BitMap.h
+++ app/include/Rendering/BitMap.h
@@ -5,9 +5,79 @@
 #ifndef APP_BITMAP_H
 #define APP_BITMAP_H
 
+/* internal includes */
+
+/* external includes */
+#include <cinttypes>
+#include <QColor>
+#include <QPixmap>
 
 class BitMap {
+    using _baseTypeT = uint8_t;
 
+    // ------------------------------
+    // Class creation
+    // ------------------------------
+public:
+    explicit BitMap(int32_t width, int32_t height);
+
+    ~BitMap();
+
+    // ------------------------------
+    // Class interaction
+    // ------------------------------
+
+    void setWhiteAll();
+
+    [[nodiscard]] QColor colorAt(const int32_t x, const int32_t y) const {
+        return {
+            m_redMap[_atCord(x, y, m_width)],
+            m_greenMap[_atCord(x, y, m_height)],
+            m_blueMap[_atCord(x, y, m_width)]
+        };
+    }
+
+    void setRedAt(const int32_t x, const int32_t y, const _baseTypeT red) {
+        m_redMap[_atCord(x, y, m_width)] = red;
+    }
+
+    void setGreenAt(const int32_t x, const int32_t y, const _baseTypeT green) {
+        m_greenMap[_atCord(x, y, m_height)] = green;
+    }
+
+    void setBlueAt(const int32_t x, const int32_t y, const _baseTypeT blue) {
+        m_blueMap[_atCord(x, y, m_height)] = blue;
+    }
+
+    void setColorAt(const int32_t x, const int32_t y, const _baseTypeT red, const _baseTypeT green,
+                    const _baseTypeT blue) {
+        m_redMap[_atCord(x, y, m_width)] = red;
+        m_greenMap[_atCord(x, y, m_height)] = green;
+        m_blueMap[_atCord(x, y, m_width)] = blue;
+    }
+
+    void dropToPixMap(QPixmap& pixMap) const;
+
+    // ------------------------------
+    // Protected class methods
+    // ------------------------------
+
+
+protected:
+    static constexpr int32_t _atCord(const int32_t x, const int32_t y, const int32_t width) {
+        return y * width + x;
+    }
+
+    // ------------------------------
+    // Class fields
+    // ------------------------------
+
+    _baseTypeT *m_redMap{};
+    _baseTypeT *m_greenMap{};
+    _baseTypeT *m_blueMap{};
+
+    int32_t m_width{};
+    int32_t m_height{};
 };
 
 
--- app/src/BitMap.cpp
+++ app/src/BitMap.cpp
@@ -2,4 +2,44 @@
 // Created by Jlisowskyy on 07/11/24.
 //
 
+/* internal includes */
 #include "../include/Rendering/BitMap.h"
+
+/* external includes */
+#include <memory>
+#include <QPainter>
+#include <QPen>
+#include <QColor>
+
+BitMap::BitMap(const int32_t width, const int32_t height): m_redMap(static_cast<_baseTypeT *>(malloc(
+                                                               sizeof(_baseTypeT) * width * height))),
+                                                           m_greenMap(static_cast<_baseTypeT *>(malloc(
+                                                               sizeof(_baseTypeT) * width * height))),
+                                                           m_blueMap(static_cast<_baseTypeT *>(malloc(
+                                                               sizeof(_baseTypeT) * width * height))),
+                                                           m_width(width),
+                                                           m_height(height) {
+}
+
+BitMap::~BitMap() {
+    free(m_redMap);
+    free(m_greenMap);
+    free(m_blueMap);
+}
+
+void BitMap::setWhiteAll() {
+    for (size_t i = 0; i < m_width * m_height; i++) {
+        m_redMap[i] = m_greenMap[i] = m_blueMap[i] = 255;
+    }
+}
+
+void BitMap::dropToPixMap(QPixmap &pixMap) const {
+    QPainter painter(&pixMap);
+
+    for (int32_t x = 0; x < m_width; x++) {
+        for (int32_t y = 0; y < m_height; y++) {
+            painter.setPen(QPen(colorAt(x, y)));
+            painter.drawPoint(x, y);
+        }
+    }
+}

5ab527ba57180bce0233d62feda5daf28d493d02
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-11-17 16:39:19 +0100

    Init avx

--- app/include/Rendering/Texture.h
+++ app/include/Rendering/Texture.h
@@ -20,9 +20,13 @@
 #include <QDebug>
 #include <QMatrix3x3>
 
+#include <immintrin.h>
+
 class Texture : public QObject {
     Q_OBJECT
 
+    using vAVX = std::array<float, 8>;
+    using vAVXPointst = std::array<vAVX, 3>;
 public:
     // ------------------------------
     // Class creation
@@ -85,20 +89,40 @@ protected:
         float d00;
         float d01;
         float d11;
+
+        float denom;
     };
 
     template<bool useNormals>
     [[nodiscard]] std::tuple<float, float, QVector3D>
     _interpolateFromTrianglePoint(const QVector3D &pos, const Triangle &triangle, const _drawData& drawData) const;
 
+    template<bool useNormals>
+    [[nodiscard]] std::tuple<vAVX, vAVX, vAVXPointst>
+    _interpolateFromTrianglePointAVX(vAVXPointst &points, const Triangle &triangle, const _drawData &drawData) const;
+
     [[nodiscard]] QColor _applyLightToTriangleColor(const QColor &color, const QVector3D &normalVector,
                                                     const QVector3D &pos, const QVector3D &lightPos) const;
+
+    [[nodiscard]] vAVXPointst _applyLightToTriangleColorAVX(vAVXPointst &colors, vAVXPointst &normalVectors,
+                                                         vAVXPointst &pos, const QVector3D &lightPos) const;
 
     template<bool useNormals, typename ColorGetterT>
     [[nodiscard]] QColor _processColor(ColorGetterT colorGetter, const QVector3D &pos, const Triangle &triangle,
                                        const QVector3D &lightPos, const _drawData& drawData) const;
 
+    template<bool useNormals, typename ColorGetterT>
+    [[nodiscard]] vAVXPointst _processColorAVX(ColorGetterT colorGetter, vAVXPointst &points, const Triangle &triangle,
+                                            const QVector3D &lightPos, const _drawData &drawData, int numValues) const;
+
+
     [[nodiscard]] static _drawData _preprocess(const Triangle& triangle);
+
+    [[nodiscard]] static __m256 *cast(vAVX &points) {
+        return reinterpret_cast<__m256 *>(points.data());
+    }
+
+    void _normalizeAVX(vAVXPointst &points) const;
 
     // ------------------------------
     // Class fields
@@ -125,7 +149,7 @@ void Texture::fillPixmap(QPixmap &pixmap, const Mesh &mesh, ColorGetterT colorGe
     BitMap bitMap(pixmap.width(), pixmap.height());
     bitMap.setWhiteAll();
 
-    #pragma omp parallel for schedule(static)
+//    #pragma omp parallel for schedule(static)
     for (const auto &triangle: mesh.getMeshArr()) {
         colorPolygon<useNormals>(bitMap, zBuffer, colorGetter, triangle, lightPos);
     }
@@ -156,6 +180,7 @@ void Texture::fillPixmap(QPixmap &pixmap, const Mesh &mesh, ColorGetterT colorGe
 template<bool useNormals, typename ColorGetterT, size_t N>
 void Texture::colorPolygon(BitMap &bitMap, int16_t *zBuffer, ColorGetterT colorGet, const PolygonArr<N> &polygon,
                            const QVector3D &lightPos) const {
+
     std::array<size_t, N> sorted{};
     for (size_t i = 0; i < N; i++) {
         sorted[i] = i;
@@ -220,27 +245,53 @@ void Texture::colorPolygon(BitMap &bitMap, int16_t *zBuffer, ColorGetterT colorG
             float zRight = std::next(it)->z;
             float zStep = (x2 - x1) != 0 ? (zRight - zLeft) / static_cast<float>(x2 - x1) : 0.0f;
 
-            for (int x = x1; x <= x2; x++) {
-                float z = zLeft + static_cast<float>(x - x1) * zStep;
+            for (int x = x1; x <= x2;) {
+                const int dist = x2 - x;
+                const int offset = std::clamp(dist, 1, 8);
 
-                const int screenX = x + bitMap.width() / 2;
-                const int screenY = scanLineY + bitMap.height() / 2;
+                alignas(32) vAVXPointst points{};
 
-                if (screenX >= 0 && screenX < bitMap.width() && screenY >= 0 && screenY < bitMap.height()) {
-                    if (const auto zRounded = static_cast<int16_t>(std::floor(z));
-                        zRounded > zBuffer[screenY * bitMap.width() + screenX]) {
-                        zBuffer[screenY * bitMap.width() + screenX] = zRounded;
-
-                        const QVector3D drawPoint{
-                            static_cast<float>(x),
-                            static_cast<float>(scanLineY),
-                            z
-                        };
-
-                        const QColor color = _processColor<useNormals>(colorGet, drawPoint, polygon, lightPos, drawData);
-                        bitMap.setColorAt(screenX, screenY, color);
-                    }
+                for (int i = 0; i < offset; ++i) {
+                    points[0][i] = static_cast<float>(x + i);
                 }
+
+                for (int i = 0; i < offset; ++i) {
+                    points[1][i] = static_cast<float>(scanLineY);
+                }
+
+                for (int i = 0; i < offset; ++i) {
+                    points[2][i] = zLeft + static_cast<float>(x - x1 + i) * zStep;
+                }
+
+                alignas(32) const auto colors = _processColorAVX<useNormals>(colorGet, points, polygon, lightPos, drawData, offset);
+
+                alignas(32) vAVX screenX{};
+                *cast(screenX) = _mm256_add_ps(*cast(points[0]),
+                                               _mm256_set1_ps(static_cast<float>(bitMap.width()) / 2.0f));
+
+                alignas(32) vAVX screenY{};
+                *cast(screenY) = _mm256_add_ps(*cast(points[1]), _mm256_set1_ps(
+                        static_cast<float>(scanLineY) + static_cast<float>(bitMap.height()) / 2.0f));
+
+                for (int i = 0; i < offset; ++i) {
+                    QVector3D pos = QVector3D(points[0][i], points[1][i], points[2][i]);
+
+                    if (screenX[i] >= 0 && screenX[i] < bitMap.width() && screenY[i] >= 0 &&
+                        screenY[i] < bitMap.height()) {
+                        if (const auto zRounded = static_cast<int16_t>(std::floor(pos.z()));
+                                zRounded >
+                                zBuffer[static_cast<int>(screenY[i]) * bitMap.width() + static_cast<int>(screenX[i])]) {
+
+                            zBuffer[static_cast<int>(screenY[i]) * bitMap.width() +
+                                    static_cast<int>(screenX[i])] = zRounded;
+
+
+                        }
+                    }
+
+                }
+
+                x += offset;
             }
 
             std::advance(it, 2);
@@ -257,38 +308,40 @@ void Texture::colorPolygon(BitMap &bitMap, int16_t *zBuffer, ColorGetterT colorG
         });
     }
 
-    for (size_t i = 0; i < N; i++) {
-        const auto &v1 = polygon[i].rotatedPosition;
-        const auto &v2 = polygon[(i + 1) % N].rotatedPosition;
+//    for (size_t i = 0; i < N; i++) {
+//        const auto &v1 = polygon[i].rotatedPosition;
+//        const auto &v2 = polygon[(i + 1) % N].rotatedPosition;
+//
+//        if (std::abs(v1.y() - v2.y()) <= 1.0f) {
+//            const int y = static_cast<int>(std::floor(v1.y()));
+//            const int x1 = static_cast<int>(std::floor(std::min(v1.x(), v2.x())));
+//            const int x2 = static_cast<int>(std::floor(std::max(v1.x(), v2.x())));
+//
+//            const float zStart = v1.z();
+//            const float zEnd = v2.z();
+//            const float zStep = x2 - x1 != 0 ? (zEnd - zStart) / (x2 - x1) : 0.0f;
+//
+//            for (int x = x1; x <= x2; ++x) {
+//                float z = zStart + (x - x1) * zStep;
+//
+//                const int screenX = x + bitMap.width() / 2;
+//                const int screenY = y + bitMap.height() / 2;
+//
+//                if (screenX >= 0 && screenX < bitMap.width() && screenY >= 0 && screenY < bitMap.height()) {
+//                    const QVector3D drawPoint{
+//                        static_cast<float>(x),
+//                        static_cast<float>(scanLineY),
+//                        z
+//                    };
+//
+//                    const QColor color = _processColor<useNormals>(colorGet, drawPoint, polygon, lightPos, drawData);
+//                    bitMap.setColorAt(screenX, screenY, color);
+//                }
+//            }
+//        }
+//    }
 
-        if (std::abs(v1.y() - v2.y()) <= 1.0f) {
-            const int y = static_cast<int>(std::floor(v1.y()));
-            const int x1 = static_cast<int>(std::floor(std::min(v1.x(), v2.x())));
-            const int x2 = static_cast<int>(std::floor(std::max(v1.x(), v2.x())));
-
-            const float zStart = v1.z();
-            const float zEnd = v2.z();
-            const float zStep = x2 - x1 != 0 ? (zEnd - zStart) / (x2 - x1) : 0.0f;
-
-            for (int x = x1; x <= x2; ++x) {
-                float z = zStart + (x - x1) * zStep;
-
-                const int screenX = x + bitMap.width() / 2;
-                const int screenY = y + bitMap.height() / 2;
-
-                if (screenX >= 0 && screenX < bitMap.width() && screenY >= 0 && screenY < bitMap.height()) {
-                    const QVector3D drawPoint{
-                        static_cast<float>(x),
-                        static_cast<float>(scanLineY),
-                        z
-                    };
-
-                    const QColor color = _processColor<useNormals>(colorGet, drawPoint, polygon, lightPos, drawData);
-                    bitMap.setColorAt(screenX, screenY, color);
-                }
-            }
-        }
-    }
+    return;
 }
 
 template<bool useNormals, typename ColorGetterT>
@@ -299,6 +352,25 @@ QColor Texture::_processColor(ColorGetterT colorGetter, const QVector3D &pos, co
     return _applyLightToTriangleColor(color, interpolatedNormalVector, pos, lightPos);
 }
 
+template<bool useNormals, typename ColorGetterT>
+Texture::vAVXPointst
+Texture::_processColorAVX(ColorGetterT colorGetter, Texture::vAVXPointst &points, const Triangle &triangle,
+                       const QVector3D &lightPos, const Texture::_drawData &drawData, int numValues) const {
+    alignas(32) auto [u, v, interpolatedNormalVector] = _interpolateFromTrianglePointAVX<useNormals>(points, triangle, drawData);
+
+    alignas(32) vAVXPointst colors{};
+
+    for (int i = 0; i < numValues; ++i) {
+        const QColor color = colorGetter(u[i], v[i]);
+
+        colors[0][i] = static_cast<float>(color.red());
+        colors[1][i] = static_cast<float>(color.green());
+        colors[2][i] = static_cast<float>(color.blue());
+    }
+
+    return _applyLightToTriangleColorAVX(colors, interpolatedNormalVector, points, lightPos);
+}
+
 template<bool useNormals>
 std::tuple<float, float, QVector3D>
 Texture::_interpolateFromTrianglePoint(const QVector3D &pos, const Triangle &triangle, const _drawData& drawData ) const {
@@ -307,9 +379,8 @@ Texture::_interpolateFromTrianglePoint(const QVector3D &pos, const Triangle &tri
     const float d20 = QVector3D::dotProduct(v2, drawData.v0);
     const float d21 = QVector3D::dotProduct(v2, drawData.v1);
 
-    const float denom = drawData.d00 * drawData.d11 - drawData.d01 * drawData.d01;
-    const float v = (drawData.d11 * d20 - drawData.d01 * d21) / denom;
-    const float w = (drawData.d00 * d21 - drawData.d01 * d20) / denom;
+    const float v = (drawData.d11 * d20 - drawData.d01 * d21) / drawData.denom;
+    const float w = (drawData.d00 * d21 - drawData.d01 * d20) / drawData.denom;
     const float u = 1.0f - v - w;
 
     const float interpolatedU =
@@ -355,6 +426,72 @@ Texture::_interpolateFromTrianglePoint(const QVector3D &pos, const Triangle &tri
         );
 
         interpolatedNormalVector.normalize();
+    }
+
+    return {interpolatedU, interpolatedV, interpolatedNormalVector};
+}
+
+template<bool useNormals>
+std::tuple<Texture::vAVX, Texture::vAVX, Texture::vAVXPointst>
+Texture::_interpolateFromTrianglePointAVX(Texture::vAVXPointst &points, const Triangle &triangle,
+                                       const Texture::_drawData &drawData) const {
+    alignas(32) vAVXPointst v2{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(v2[i]) = _mm256_sub_ps(*cast(points[i]), _mm256_set1_ps(triangle[0].rotatedPosition[i]));
+    }
+
+    alignas(32) vAVX d20{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(d20) = _mm256_fmadd_ps(*cast(v2[i]), _mm256_set1_ps(drawData.v0[i]), *cast(d20));
+    }
+
+    alignas(32) vAVX d21{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(d21) = _mm256_fmadd_ps(*cast(v2[i]), _mm256_set1_ps(drawData.v1[i]), *cast(d21));
+    }
+
+    alignas(32) vAVX vV{};
+
+    *cast(vV) = _mm256_sub_ps(_mm256_mul_ps(_mm256_set1_ps(drawData.d11), *cast(d20)),
+                              _mm256_mul_ps(_mm256_set1_ps(drawData.d01), *cast(d21)));
+    *cast(vV) = _mm256_div_ps(*cast(vV), _mm256_set1_ps(drawData.denom));
+
+    alignas(32) vAVX vW{};
+
+    *cast(vW) = _mm256_sub_ps(_mm256_mul_ps(_mm256_set1_ps(drawData.d00), *cast(d21)),
+                              _mm256_mul_ps(_mm256_set1_ps(drawData.d01), *cast(d20)));
+    *cast(vW) = _mm256_div_ps(*cast(vW), _mm256_set1_ps(drawData.denom));
+
+    alignas(32) vAVX vU{};
+    *cast(vU) = _mm256_sub_ps(_mm256_set1_ps(1.0f), _mm256_add_ps(*cast(vV), *cast(vW)));
+
+    alignas(32) vAVX interpolatedU{};
+    *cast(interpolatedU) = _mm256_fmadd_ps(*cast(vU), _mm256_set1_ps(triangle[0].u), *cast(interpolatedU));
+    *cast(interpolatedU) = _mm256_fmadd_ps(*cast(vV), _mm256_set1_ps(triangle[1].u), *cast(interpolatedU));
+    *cast(interpolatedU) = _mm256_fmadd_ps(*cast(vW), _mm256_set1_ps(triangle[2].u), *cast(interpolatedU));
+
+    alignas(32) vAVX interpolatedV{};
+    *cast(interpolatedV) = _mm256_fmadd_ps(*cast(vU), _mm256_set1_ps(triangle[0].v), *cast(interpolatedV));
+    *cast(interpolatedV) = _mm256_fmadd_ps(*cast(vV), _mm256_set1_ps(triangle[1].v), *cast(interpolatedV));
+    *cast(interpolatedV) = _mm256_fmadd_ps(*cast(vW), _mm256_set1_ps(triangle[2].v), *cast(interpolatedV));
+
+    for (int i = 0; i < 8; ++i) {
+        interpolatedU[i] = std::clamp(interpolatedU[i], 0.0f, 1.0f);
+        interpolatedV[i] = std::clamp(interpolatedV[i], 0.0f, 1.0f);
+    }
+
+    alignas(32) vAVXPointst interpolatedNormalVector{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(interpolatedNormalVector[i]) = _mm256_fmadd_ps(*cast(vU), _mm256_set1_ps(triangle[0].rotatedNormal[i]),
+                                                             *cast(interpolatedNormalVector[i]));
+        *cast(interpolatedNormalVector[i]) = _mm256_fmadd_ps(*cast(vV), _mm256_set1_ps(triangle[1].rotatedNormal[i]),
+                                                             *cast(interpolatedNormalVector[i]));
+        *cast(interpolatedNormalVector[i]) = _mm256_fmadd_ps(*cast(vW), _mm256_set1_ps(triangle[2].rotatedNormal[i]),
+                                                             *cast(interpolatedNormalVector[i]));
     }
 
     return {interpolatedU, interpolatedV, interpolatedNormalVector};
--- app/src/Texture.cpp
+++ app/src/Texture.cpp
@@ -61,5 +61,96 @@ Texture::_drawData Texture::_preprocess(const Triangle &triangle) {
     result.d01 = QVector3D::dotProduct(result.v0, result.v1);
     result.d11 = QVector3D::dotProduct(result.v1, result.v1);
 
+    result.denom = result.d00 * result.d11 - result.d01 * result.d01;
+
     return result;
 }
+
+Texture::vAVXPointst
+Texture::_applyLightToTriangleColorAVX(Texture::vAVXPointst &colors, Texture::vAVXPointst &normalVectors,
+                                    Texture::vAVXPointst &pos, const QVector3D &lightPos) const {
+    static constexpr QVector3D V(0, 0, 1);
+
+    _normalizeAVX(normalVectors);
+
+    alignas(32) vAVXPointst toLightVectors{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(toLightVectors[i]) = _mm256_sub_ps(*cast(pos[i]), _mm256_set1_ps(lightPos[i]));
+    }
+
+    alignas(32) vAVX NdotL{};
+    for (int i = 0; i < 3; ++i) {
+        *cast(NdotL) = _mm256_fmadd_ps(*cast(normalVectors[i]), *cast(toLightVectors[i]), *cast(NdotL));
+    }
+
+    alignas(32) vAVXPointst R{};
+    alignas(32) vAVX NdotL2{};
+    *cast(NdotL2) = _mm256_mul_ps(*cast(NdotL), _mm256_set1_ps(2.0f));
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(R[i]) = _mm256_fmsub_ps(*cast(NdotL2), *cast(normalVectors[i]), *cast(toLightVectors[i]));
+    }
+
+    _normalizeAVX(R);
+
+    alignas(32) vAVX VdotR{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(VdotR) = _mm256_fmadd_ps(_mm256_set1_ps(V[i]), *cast(R[i]), *cast(VdotR));
+    }
+
+    alignas(32) vAVX cos0{};
+    *cast(cos0) = _mm256_max_ps(*cast(NdotL), _mm256_set1_ps(0.0f));
+
+    alignas(32) vAVX cos1{};
+    *cast(cos1) = _mm256_max_ps(*cast(VdotR), _mm256_set1_ps(0.0f));
+
+    alignas(32) vAVX cos1m{};
+    *cast(cos1m) = _mm256_set1_ps(1.0f);
+    for (int i = 0; i < static_cast<int>(m_mCoef); ++i) {
+        *cast(cos1m) = _mm256_mul_ps(*cast(cos1m), *cast(cos1));
+    }
+
+    QVector3D lightColors = QVector3D(
+            static_cast<float>(m_lightColor.red()),
+            static_cast<float>(m_lightColor.green()),
+            static_cast<float>(m_lightColor.blue())) / 255.0f;
+
+    alignas(32) vAVXPointst lightColorsAVX{};
+
+    for (int i = 0; i < 3; ++i) {
+        alignas(32) vAVX left{};
+        *cast(left) = _mm256_mul_ps(_mm256_set1_ps(m_kdCoef), _mm256_set1_ps(lightColors[i]));
+        *cast(left) = _mm256_mul_ps(*cast(left), *cast(colors[i]));
+        *cast(left) = _mm256_mul_ps(*cast(left), *cast(cos0));
+
+        alignas(32) vAVX right{};
+        *cast(right) = _mm256_mul_ps(_mm256_set1_ps(m_ksCoef), _mm256_set1_ps(lightColors[i]));
+        *cast(right) = _mm256_mul_ps(*cast(right), *cast(colors[i]));
+        *cast(right) = _mm256_mul_ps(*cast(right), *cast(cos1m));
+
+        *cast(lightColorsAVX[i]) = _mm256_add_ps(*cast(left), *cast(right));
+        *cast(lightColorsAVX[i]) = _mm256_max_ps(*cast(lightColorsAVX[i]), _mm256_set1_ps(0.0f));
+        *cast(lightColorsAVX[i]) = _mm256_min_ps(*cast(lightColorsAVX[i]), _mm256_set1_ps(1.0f));
+
+        *cast(lightColorsAVX[i]) = _mm256_mul_ps(*cast(lightColorsAVX[i]), _mm256_set1_ps(255.0f));
+    }
+
+    return lightColorsAVX;
+}
+
+void Texture::_normalizeAVX(Texture::vAVXPointst &points) const {
+    alignas(32) vAVX lengths{};
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(lengths) = _mm256_fmadd_ps(*cast(points[i]), *cast(points[i]), *cast(lengths));
+    }
+
+    *cast(lengths) = _mm256_sqrt_ps(*cast(lengths));
+
+    for (int i = 0; i < 3; ++i) {
+        *cast(points[i]) = _mm256_div_ps(*cast(points[i]), *cast(lengths));
+    }
+}
+
